81b1223a3ae9a12cf2551403a5299a99a40f61a3
Shail <shailshah.officework@gmail.com>
2025-02-23 21:11:02 +0530

    added auth module and setup route in microfrontend

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import ProgressBar from "./components/Progress";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
+
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <Suspense fallback={<ProgressBar />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
